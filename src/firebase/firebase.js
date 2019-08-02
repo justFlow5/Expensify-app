8765fcde1e19b05c7e01f38c63024e81aa0879f9
justFlow5 <justflow555@gmail.com>
2019-08-02 09:23:22 +0200

    Setup production build and server

---
+++ src/firebase/firebase.js
@@ -0,0 +1,131 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
+  };
+
+
+
+firebase.initializeApp(firebaseConfig);
+
+
+const database = firebase.database();
+
+export { firebase, database as default};
+
+
+
+
+// database.ref().set({
+//   name: 'Andrew Mead',
+//   age: 26,
+//   isSingle: false,
+//   location: {
+//     city: 'Philadelphia',
+//     country: 'United States'
+//   },
+//   job: {
+//       title: 'software developer',
+//       company: 'Amazon'
+//   }
+// }).then(() => {
+//   console.log('Data is saved!');
+// }).catch((e) => {
+//   console.log('This failed.', e);
+// });
+
+// // database.ref().set('This is my data.');
+
+// // database.ref('age').set(27);
+// // database.ref('location/city').set('New York');
+
+// // database.ref('attributes').set({
+// //   height: 73,
+// //   weight: 150
+// // }).then(() => {
+// //   console.log('Second set call worked.');
+// // }).catch((e) => {
+// //   console.log('Things didnt for the second error', e);
+// // });
+
+// // database.ref('isSingle')
+// //     .remove()
+// //     .then(() => {
+// //          console.log('The item has been removed');
+// //     }).catch((err) => {
+// //         console.log("This failed.", err);
+// //     })
+
+
+//     // database.ref().on('value', (snapshot) => {
+//     //     const name = database.ref('name').once('value');
+//     //     const title = database.ref('job/title').once('value');
+//     //     const company = database.ref('job/company').once('value');
+//     //     console.log(`${name} is a ${title} at ${company}.`);
+//     // }, (err) => {
+//     //     console.log('This FAILED', err);
+//     // });
+
+//     // setTimeout(() => {
+//     //     database.ref('name').set('Michael');
+//     // },
+//     // 3000);
+
+//     // setTimeout(() => {
+//     //     database.ref('job/title').set('web-developer');
+//     // },
+//     // 6000);
+
+//     // setTimeout(() => {
+//     //     database.ref('job/company').set('Google');
+//     // },
+//     // 9000);
+
+    
+//     database.ref('expense').push({
+//         description: 'electricity',
+//         note: '',
+//         amount: 5000,
+//         createdAt: 1003332100
+//     });
+
+//     database.ref('expense').push({
+//         description: 'rent',
+//         note: '',
+//         amount: 1092,
+//         createdAt: 10000
+//     });
+
+//     database.ref('expense').push({
+//         description: 'rent',
+//         note: '',
+//         amount: 320,
+//         createdAt: 10001200011
+//     })
+
+//     // database.ref('expense').on('value', (snapshot) => {
+//     //     const expenses = [];
+
+//     //     snapshot.forEach(childSnapshot => {
+//     //         const id = childSnapshot.key;
+//     //         expenses.push(
+//     //             {
+//     //                 id,
+//     //                 ...childSnapshot.val()
+//     //             }
+//     //         );
+//     //         console.log(expenses);
+//     //     })
+//     // }, (err) => {
+//     //     console.log('This failed.', err);
+//     // })
+
+//     database.ref('expense').on('child_changed', (snapshot) => {
+//         console.log(snapshot.key, snapshot.val());
+//     })

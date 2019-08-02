b79b51264e47282bf6f40ae7b850bd21d90641f8
justFlow5 <justflow555@gmail.com>
2019-08-02 09:55:04 +0200

    webbpack.config.js bug fixed

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-if (process.env.NODE_ENV === test) {
+if (process.env.NODE_ENV === 'test') {
   require('dotenv').config({path: '.env.test'})
 } else if (process.env.NODE_ENV === 'development') {
   require('dotenv').config({path: '.env.development'})
